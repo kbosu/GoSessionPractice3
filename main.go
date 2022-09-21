4ffbf8564ceaa9091477ef821dc82e3ca243414d
kbosu <113999398+kbosu@users.noreply.github.com>
2022-09-21 14:00:56 +0530

    Update main.go

--- main.go
+++ main.go
@@ -6,21 +6,6 @@ import (
 )
 
 func main() {
-	fmt.Println("This program is going to help the engineer understand different data types")
-	// var myInt int32
-	// var myFloat float32
-	// var myString string
-	// const myConstant = 55
-	/*for j := 1; j != 17; j++ {
-		fmt.Println(j)
-	}*/
-	/*mySlice := []int32{1, 2, 3, 4, 5}
-	for i := 0; i < len(mySlice); i++ {
-		fmt.Println(mySlice[i])
-	}
-	for j := range mySlice {
-		fmt.Println(mySlice[j])
-	}*/
 	mySliceString := []string{"manish", "temp", "you", "here", "while", "for", "temp", "manish", "you"}
 	myMap := make(map[string]int, 0)
 	for s := range mySliceString {
